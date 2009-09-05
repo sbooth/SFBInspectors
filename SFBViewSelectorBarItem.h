b72b68f4d7d37931ed9a1c39e55921d14dc45227
Stephen F. Booth <me@sbooth.org>
2009-09-04 23:20:21 -0700

    Reordered definitions

--- SFBViewSelectorBarItem.h
+++ SFBViewSelectorBarItem.h
@@ -15,13 +15,13 @@
 	NSView *_view;
 }
 
-+ (id) itemWithIdentifier:(NSString *)identifier label:(NSString *)label tooltip:(NSString *)tooltip image:(NSImage *)image view:(NSView *)view;
-
 @property (copy) NSString * identifier;
 @property (copy) NSString * label;
 @property (copy) NSString * tooltip;
 @property (copy) NSImage * image;
 @property (retain) NSView * view;
+
++ (id) itemWithIdentifier:(NSString *)identifier label:(NSString *)label tooltip:(NSString *)tooltip image:(NSImage *)image view:(NSView *)view;
 
 - (id) initWithIdentifier:(NSString *)identifier label:(NSString *)label tooltip:(NSString *)tooltip image:(NSImage *)image view:(NSView *)view;
 
